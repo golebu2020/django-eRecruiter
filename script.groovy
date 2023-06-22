9663f54791e849c094c4454f927474713760a4cb
Chinedu Olebu <cgolebu@gmail.com>
2023-06-21 22:23:35 -0700

    modified script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,14 @@
 def buildApp(){
     echo "building and pushing..."
     sh "docker build --tag django-erecruiter:1.1.0 ."
+    sh "docker tag django-erecruiter:1.1.0 46.101.168.73:8082/django-erecruiter:1.1.0"
     push()
 }
 return this
 
 def push(){
+    sh "docker login -u chinedu -p Nedu123@# 46.101.168.73:8082"
+    sh "docker push 46.101.168.73:8082/django-erecruiter:1.1.0"
     echo "Pushing..."
 
 }
